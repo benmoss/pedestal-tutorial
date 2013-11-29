390460624a0ddbc6579cefab6a19add8b00c9e4c
Ben Moss <ben@mossity.com>
2013-11-29 13:26:40 -0500

    Add a transform to the counter

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -1,10 +1,16 @@
 (ns ^:shared tutorial-client.behavior
     (:require [clojure.string :as string]
+              [io.pedestal.app :as app]
               [io.pedestal.app.messages :as msg]))
 
 (defn inc-transform [old-value message]
   ((fnil inc  0) old-value))
 
+(defn init-main [_]
+  [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
+
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]
+   :emit [{:init init-main}
+          [#{[:*]} (app/default-emitter [:main])]]})
