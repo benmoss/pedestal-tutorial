15b98e3f88ef3403523c0018d457c446253fce7d
Ben Moss <ben@mossity.com>
2013-11-30 22:20:30 -0500

    Add some debugging and some post-processing

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -27,16 +27,31 @@
 (defn merge-counters [_ {:keys [me others]}]
   (assoc others "Me" me))
 
+(defn cumulative-average [debug key x]
+  (let [k (last key)
+        i (inc (or (::avg-count debug) 0))
+        avg (or (::avg-raw debug) 0)
+        new-avg (+ avg (/ (- x avg) i))]
+    (assoc debug
+           ::avg-count i
+           ::avg-raw new-avg
+           (keyword (str (name k) "-avg")) (int new-avg))))
+
 (def example-app
   {:version 2
+   :debug true
    :transform [[:inc [:my-counter] inc-transform]
-               [:swap [:**]        swap-transform]]
+               [:swap [:**]        swap-transform]
+               [:debug [:pedestal :**] swap-transform]]
    :emit [{:init init-main}
           [#{[:my-counter]
              [:other-counters :*]
              [:total-count]
              [:max-count]
-             [:average-count]} (app/default-emitter [:main])]]
+             [:average-count]} (app/default-emitter [:main])]
+          [#{[:pedestal :debug :dataflow-time]
+             [:pedestal :debug :dataflow-time-max]
+             [:pedestal :debug :dataflow-time-avg]} (app/default-emitter [])]]
    :derive #{[{[:my-counter] :me
                [:other-counters] :others}
               [:counters] merge-counters :map]
@@ -44,5 +59,13 @@
              [#{[:counters :*]} [:max-count] maximum :vals]
              [{[:counters :*] :nums
                [:total-count] :total} [:average-count] average-count :map]
-             }
+             [#{[:pedestal :debug :dataflow-time]}
+              [:pedestal :debug :dataflow-time-max] maximum :vals]
+             [#{[:pedestal :debug :dataflow-time]}
+              [:pedestal :debug] cumulative-average :map-seq]}
    :effect #{[#{[:my-counter]} publish-counter :single-val]}})
+
+(defn add-post-processors [dataflow]
+  (-> dataflow
+      (update-in [:post :app-model] (fnil conj [])
+                 [:value [:main :average-count] round-number-post])))
