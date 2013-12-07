6eaf7cd0ce408a614c6498a9733bad05385f004b
Ben Moss <ben@mossity.com>
2013-12-07 15:27:38 -0500

    WIP rendering

--- app/src/tutorial_client/rendering.cljs
+++ app/src/tutorial_client/rendering.cljs
@@ -2,21 +2,20 @@
   (:require [domina :as dom]
             [io.pedestal.app.render.push :as render]
             [io.pedestal.app.render.push.templates :as templates]
-            [io.pedestal.app.render.push.handlers.automatic :as d])
+            [io.pedestal.app.render.push.handlers.automatic :as d]
+            [io.pedestal.app.render.push.handlers :as h])
   (:require-macros [tutorial-client.html-templates :as html-templates]))
 
 (def templates (html-templates/tutorial-client-templates))
 
-(defn render-page [renderer [_ path] transmitter]
+(defn render-template [renderer [_ path] _]
   (let [parent (render/get-parent-id renderer path)
         id (render/new-id! renderer path)
         html (templates/add-template renderer path (:tutorial-client-page templates))]
-    (dom/append! (dom/by-id parent) (html {:id id :message ""}))))
-
-(defn render-message [renderer [_ path _ new-value] transmitter]
-  (templates/update-t renderer path {:message new-value}))
+    (dom/append! (dom/by-id parent) (html {:id id}))))
 
 (defn render-config []
-  [[:node-create  [:greeting] render-page]
-   [:node-destroy   [:greeting] d/default-exit]
-   [:value [:greeting] render-message]])
+  [[:node-create [:main] render-template]
+   [:node-destroy [:main] h/default-destroy]
+   [:transform-enable [:main :my-counter] (h/add-send-on-click "inc-button")]
+   [:transform-disable [:main :my-counter] (h/remove-send-on-click "inc-button")]])
